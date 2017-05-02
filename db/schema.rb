976a160d931e9120da6c9de455d6707ca308c55d
Sergiiss <Dracula23@mail.ru>
2017-05-02 21:41:11 +0300

    Add user login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170426113052) do
+ActiveRecord::Schema.define(version: 20170502173511) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "post_id"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 20170426113052) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
