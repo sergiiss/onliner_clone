80d3ce817442993e8a403e6f1720ed6a80db3782
Yuri Kasperovich <ykas.gg@gmail.com>
2017-05-23 19:24:24 +0300

    Update comment.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,10 @@
 class Comment < ApplicationRecord
   has_many   :likes
-
   belongs_to :post
-  validates  :name, presence: true
+  
+  validates :name, presence: true
+  
+  def from?(user)
+    likes.where(user_id: user.id).present?
+  end
 end
