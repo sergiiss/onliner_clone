945983905d1e60eeede49778dab6a43bc4dbfbda
Yuri Kasperovich <ykas.gg@gmail.com>
2017-07-04 19:57:14 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,18 @@
 Rails.application.routes.draw do
   resources :users
 
-  resource :sessions, only: [:new, :create ]
-
+  resource :sessions, only: [ :new, :create ]
   get '/logout', to: "sessions#destroy", as: "logout"
-  get '/list', to: "posts#list", as: "list"
-
+  
   resources :posts do
     resources :comments
   end
-
+  
+  get '/list', to: "posts#list", as: "list"
+  
   resources :comments do
     resources :likes
   end
 
   root to: "posts#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
