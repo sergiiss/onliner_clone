e3a661de2d4051ed52d4f1dc8368efbd77b06453
Sergiiss <Dracula23@mail.ru>
2017-08-02 22:33:08 +0300

    Add method truncate for minor_posts and secondary_posts

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,13 +1,4 @@
 module PostsHelper
-  def post_title_for_visual(posts, index)
-    text_title = posts[index].title
-    if text_title.length > 70
-      text_title = text_title[0..69] + '...'
-    else
-      text_title
-    end
-  end
-
   def сategory_selection
     {
       'Люди': :people, 'Мнения':     :opinions,
