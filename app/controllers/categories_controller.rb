1b24eaf1e1275ccb162af984774e3a3c515a1237
Sergiiss <Dracula23@mail.ru>
2017-08-10 00:02:49 +0300

    Minor improvement to code

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,6 @@
 class CategoriesController < ApplicationController
+  skip_before_action :authenticate_user, only: [ :priority_list ]
+
   def index
     @categories = Category.all
   end
@@ -9,6 +11,20 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+
+    if @category.update_attributes(category_params)
+      redirect_to @category
+    else
+      render :edit
+    end
   end
 
   def change_priority
